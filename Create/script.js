c47f1e6b5023941040b431875c73c9ad354feaf6
Ellen Seghers <ellen.seghers1@gmail.com>
2022-01-11 15:54:08 +0100

    Last commit

--- Create/script.js
+++ Create/script.js
@@ -2,11 +2,24 @@ var usedColors = [];
 var articleVar = document.querySelector('article');
 var studentNames = ['Ahmad', 'Jian', 'Michael', 'Dante', 'Muhammet', 'Nicolas', 'Jordy', 'Beryl', 'Cynthia', 'Ruben', 'Magali', 'Jawid', 'Koen', 'Kevin', 'Benjamin', 'Deryl', 'Michelle', 'Colin', 'Feruz', 'Pieter', 'Ellen', 'Sara'];
 
-for(var i = 0; i < studentNames.length; i++){
+var randomNames = [];
+var numberNames = studentNames.length;
+for(var i = 0; i < numberNames; i++){
+	var rand = Math.floor(Math.random() * (studentNames.length -1));
+	randomNames.push(studentNames.splice(rand, 1));
+}
+
+
+for(var i = 0; i < randomNames.length; i++){
 	var newSection = document.createElement("SECTION");
-	newSection.style.backgroundColor = colorpicker();
+	var backgroundColor = colorpicker();
+	newSection.style.backgroundColor = backgroundColor;
+	var textColor = lightOrDark(backgroundColor);
+
 	var newParagraph = document.createElement('P');
-	newParagraph.innerText = studentNames[i];
+	newParagraph.innerText = randomNames[i];
+	newParagraph.style.color = textColor;
+
 	newSection.append(newParagraph);
 	articleVar.append(newSection);
 	console.log(newSection);
@@ -23,4 +36,37 @@ function colorpicker() {
 	}
 	usedColors.push(color); 
 	return color;
+}
+
+function lightOrDark(color) {
+
+    // Variables for red, green, blue values
+    let red;
+    let green;
+    let blue;
+    let hsp;
+    
+    // If hex --> Convert it to RGB: http://gist.github.com/983661
+    //color = "#2FA25C";
+    color = +("0x" + color.slice(1).replace( 
+    color.length < 5 && /./g, '$&$&'));
+
+    red = color >> 16;
+    green = color >> 8 & 255;
+    blue = color & 255;
+    
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(
+    0.299 * (red * red) +
+    0.587 * (green * green) +
+    0.114 * (blue * blue)
+    );
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp>127.5) {
+        return color = "black";
+    } 
+    else {
+        return color = "white";
+    }
 }
