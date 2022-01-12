62ede1e669f32026d39294236309348d28600a77
Ellen Seghers <ellen.seghers1@gmail.com>
2022-01-12 16:52:02 +0100

    added pictures in card data

--- TheCollection/script.js
+++ TheCollection/script.js
@@ -4,106 +4,79 @@ const collection = [
 		genre: ['Pop', 'Soul', 'R&B',],
 		yearsActive: '1963-2009',
 		country: 'United States',
-		picture: '',
+		picture: "buy-michael-jackson-bad-tour-belt.jpg",
 		cast: ['Jennifer Batten', 'Slash', 'Judith Glory Hill',]
 	},
-]
-
-const collection = [
 	{
 		name: 'Smash Into Pieces',
 		genre: 'Alternative Rock',
 		yearsActive: '2008-today',
 		country: 'Sweden',
-		picture: '',
+		picture: "A-3178818-1539455224-2491.jpeg.jpg",
 		cast: ['Chris Adam Hedman Sörbye', 'Benjamin Jennebo', 'The Apocalypse DJ', 'Per Bergquist',]
 	},
-]
-
-const collection = [
 	{
 		name: 'Thousand Foot Krutch',
 		genre: ['Alternative Rock',],
 		yearsActive: '1995-today',
 		country: 'Canada',
-		picture: '',
+		picture: "tfk-web-1400x932.jpg",
 		cast: ['Trevor McNevan', 'Joel Bruyere', 'Steve Augustine', 'Ty Dietzler',]
 	},
-]
-
-const collection = [
 	{
 		name: 'Apocalyptice',
 		genre: ['Alternative Metal',],
 		yearsActive: '1993-today',
 		country: 'Helsinki',
-		picture: '',
+		picture: "de1ea6e0-df03-49bb-bcb2-9d88f0709205.jpg",
 		cast: ['Eicca Toppinen', 'Paavo Lötjönen', 'Perttu Kivilaakso', 'Mikko Sirén', 'Franky Perez',]
 	},
-]
-
-const collection = [
 	{
 		name: 'Within Temptation',
 		genre: ['Symphonic Metal',],
 		yearsActive: '1996-today',
 		country: 'Netherlands',
-		picture: '',
+		picture: "uueuwmchjzigzy7kk78n.jpg",
 		cast: ['Sharon den Adel', '	Stefan Helleblad', 'Jeroen van Veen', '	Mike Coolen', 'Martijn Spierenburg',],
 	},
-]
-
-const collection = [
 	{
 		name: 'Evanescence',
 		genre: ['Alternative Rock', 'Gothic Rock',],
 		yearsActive: '1998-today',
 		country: 'Arkansas',
-		picture: '',
+		picture: "Evanescence.jpg",
 		cast: ['Amy Lee', 'Jen Majura', 'Troy McLawhorn', 'Tim McCord', 'Will Hunt',],
 	},
-]
-
-const collection = [
 	{
 		name: 'Linkin Park',
 		genre: ['Alternative Rock', 'Alternative Metal', 'Raprock',],
 		yearsActive: '1999-2017',
 		country: 'Caliornië',
-		picture: '',
+		picture: "linkin_park1-e1424877421108.jpg",
 		cast: ['Chester Bennington', 'Mike Shinoda', 'Dave "Phoenix" Farrell', 'Brad Delson', 'Joe Hahn' , 'Rob Bourdon', 'Mark Wakefield',],
 	},
-]
-
-const collection = [
 	{
 		name: 'Adam Lambert',
 		genre: ['Pop', 'Poprock', 'Rock', 'Glamrock'],
 		yearsActive: '2009-today',
 		country: 'United States',
-		picture: '',
+		picture: "Queen_And_Adam_Lambert_-_The_O2_-_Tuesday_12th_December_2017_QueenO2121217-14_(25093782907)_Cropped.jpg",
 		cast: ['/',]
 	},
-]
-
-const collection = [
 	{
 		name: 'Nickelback',
 		genre: ['Hardrock', 'Alternative Rock', 'Poprock',],
 		yearsActive: '1995-today',
 		country: 'Hanna',
-		picture: '',
+		picture: "download.jpg",
 		cast: ['Chad Kroeger', 'Ryan Peake', 'Mike Kroeger','Daniel Adair',],
 	},
-]
-
-const collection = [
 	{
 		name: 'Skillet',
 		genre: ['Alternative rock', 'Hardrock',],
 		yearsActive: '1996-today',
 		country: 'United States',
-		picture: '',
+		picture: "gmm-2020-skillet-8iusewa7kepgfbm-1.jpg",
 		cast: ['John Cooper', 'John Cooper', 'Korey Cooper', 'Jen Ledger', 'Seth Morisson',]
 	},
 ]
