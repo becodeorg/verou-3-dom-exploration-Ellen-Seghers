7106b63ac8ce10458fd0f4b32ea0a02ca8400443
Ellen Seghers <ellen.seghers1@gmail.com>
2022-01-17 14:37:04 +0100

    "Select the selector"

--- Navigate/script.js
+++ Navigate/script.js
@@ -1,5 +1,14 @@
-//TO DO ADD THE LAST CHILD OF <OL> TAG
+//TODO: Select the selector
+const section = document.querySelector('section');
+const sectionsChildren = section.children
+console.log(sectionsChildren) // [<h2> <p> <p>]
+
+//TODO: Slect the last child of the <OL> tag
 
 
-//TO DO PUT THE LAST CHILD OF <OL> AT THE BEGINNING OF THE LIST
+//TODO: Get the element that comes after the third child (<p>)
+
+
+
+//TODO: put the last child of <ol> at the beginning of the list
 
