68c494fdd6e9c38a90c20dfad69ec49c923be2f6
Ellen Seghers <ellen.seghers1@gmail.com>
2022-01-18 10:00:38 +0100

    "Created a new div"

--- Events/script.js
+++ Events/script.js
@@ -1,3 +1,5 @@
+//update exercise to modern syntax
+
 const _initTime = Date.now()
 
 function getElapsedTime(){
@@ -7,6 +9,16 @@ function getElapsedTime(){
 function clickOnSquare(e){
     console.log(e.target.classList[1])
     console.log(getElapsedTime())
+
+    //create a new div and giv it a class name (select the location where you want to add it first)
+    const containerSection = document.querySelector(".displayedsquare-wrapper");
+    const div = document.createElement("div");
+    div.classList.add("displayedsquare");
+    containerSection.append(div);
+
+    //TODO: get color
+
+    //TODO: apply color
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
